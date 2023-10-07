41281deb52238055875f00e608fcde0c9f53b869
unknown <ariveralun@gmail.com>
2023-10-07 20:39:48 +0100

    WIP vulkan config

--- include/window.h
+++ include/window.h
@@ -5,6 +5,8 @@
 #include <vector>
 #include <iostream>
 #include <string>
+#include <optional>
+
 #ifndef window_class
 #define window_class 
 
@@ -12,14 +14,56 @@ class window {
 public:
 	window();
 	~window();
+	struct QueueFamilyIndices {
+		std::optional<uint32_t> graphicsFamily;
+		std::optional<uint32_t> presentFamily;
+		uint32_t queueFamilyCount;
+
+		bool isComplete() {
+			return graphicsFamily.has_value() && presentFamily.has_value();
+		}
+	};
+
+	QueueFamilyIndices findQueueFamilies(VkPhysicalDevice physicalDevice) {
+		QueueFamilyIndices indices;
+
+		// Obtener la cantidad de familias de colas disponibles en la GPU
+		uint32_t queueFamilyCount = 0;
+		vkGetPhysicalDeviceQueueFamilyProperties(physicalDevice, &queueFamilyCount, nullptr);
+
+		// Obtener información de las familias de colas disponibles
+		std::vector<VkQueueFamilyProperties> queueFamilies(queueFamilyCount);
+		vkGetPhysicalDeviceQueueFamilyProperties(physicalDevice, &queueFamilyCount, queueFamilies.data());
+
+		// Buscar una familia de colas que admita gráficos
+		for (uint32_t i = 0; i < queueFamilyCount; ++i) {
+			if (queueFamilies[i].queueFlags & VK_QUEUE_GRAPHICS_BIT) {
+				indices.graphicsFamily = i;
+			}
+
+			// Puedes agregar más condiciones para encontrar otras familias de colas si es necesario
+		}
+
+		return indices;
+	}
+
 	void end();
 	void clean();
 
+	// Vulkan stuff
+	void initialize_vulkan();
+	void validation_layers();
+	void physical_devices();
+	void logical_devices();
+	void create_presentation_queue_and_swapchain();
+	QueueFamilyIndices indices;
 	// Getters
 	GLFWwindow* get_window();
-
+	VkSurfaceKHR get_vulkan_surface();
 	// Setters
 	void set_clear_color(float r, float g, float b, float a);
+
+	
 
 private:
 	// Window stuff
@@ -32,7 +76,20 @@ private:
 	// Vulkan stuff
 	VkInstanceCreateInfo vk_create_info_;
 	VkInstance vk_instance_;
+	VkDevice vk_device_;
 	VkPhysicalDevice vk_physical_device_;
+	VkSurfaceKHR surface_;
+	VkQueue graphicsQueue;
+	VkSurfaceFormatKHR selectedFormat;
+
+	uint32_t minImgCount;
+	uint32_t maxImgCount;
+	VkExtent2D currentExtent;
+	VkExtent2D minExtent;
+	VkExtent2D maxExtent;
+	bool supportsAsync;
+	bool presentWithCompositeAlphaSupported;
+
 };
 
 
--- src/window.cc
+++ src/window.cc
@@ -11,12 +11,51 @@ window::window()
 
 	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
 
-	window_ = glfwCreateWindow(width_, 600, "Ventana Vulkan", nullptr, nullptr);
+	window_ = glfwCreateWindow(width_, 600, "Vulkan Window", nullptr, nullptr);
 
 	if (!window_) {
-		throw std::runtime_error("Error al crear la ventana GLFW");
+		throw std::runtime_error("Error creating GLFW window");
 	}
 
+	initialize_vulkan();
+
+}
+
+window::~window()
+{
+	vkDestroyInstance(vk_instance_, nullptr);
+
+	glfwDestroyWindow(window_);
+
+	vkDestroySurfaceKHR(vk_instance_, surface_, nullptr);
+
+	glfwTerminate();
+}
+
+
+void window::end()
+{
+	glfwDestroyWindow(window_);
+	glfwTerminate();
+}
+
+void window::clean()
+{
+	VkClearAttachment clearAttachment = {};
+	clearAttachment.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+	clearAttachment.clearValue = clearColor;
+
+	VkClearRect clearRect = {};
+	clearRect.layerCount = 1;
+	clearRect.rect.extent.width = width_;
+	clearRect.rect.extent.height = height_;
+
+	vkDestroyDevice(vk_device_, nullptr);
+	//vkCmdClearAttachments()
+}
+
+void window::initialize_vulkan()
+{
 	// Here we set the info of our application 
 	vk_create_info_ = {};
 
@@ -49,6 +88,49 @@ window::window()
 		throw std::runtime_error("Error while creating Vulkan instance.");
 	}
 
+	VkSurfaceCapabilitiesKHR surfaceCapabilities;
+	if (vkGetPhysicalDeviceSurfaceCapabilitiesKHR(vk_physical_device_, surface_, &surfaceCapabilities) != VK_SUCCESS) {
+		throw std::runtime_error("Error al obtener las capacidades de la cadena de intercambio.");
+	}
+
+	minImgCount = surfaceCapabilities.minImageCount;
+	maxImgCount = surfaceCapabilities.maxImageCount;
+	currentExtent = surfaceCapabilities.currentExtent;
+	minExtent = surfaceCapabilities.minImageExtent;
+	maxExtent = surfaceCapabilities.maxImageExtent;
+
+	supportsAsync = (surfaceCapabilities.supportedCompositeAlpha & VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR) != 0;
+	presentWithCompositeAlphaSupported = (surfaceCapabilities.supportedCompositeAlpha & VK_COMPOSITE_ALPHA_PRE_MULTIPLIED_BIT_KHR) != 0;
+
+	physical_devices();
+
+	validation_layers();
+
+	logical_devices();
+
+	create_presentation_queue_and_swapchain();
+}
+
+void window::validation_layers()
+{
+	// Here we set the validation layers
+
+	uint32_t layerCount = 0;
+	vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
+	std::vector<VkLayerProperties> availableLayers(layerCount);
+	vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
+	
+	vk_create_info_.enabledLayerCount = 2;
+	const char* validationLayers[] = {
+		"VK_LAYER_KHRONOS_validation",
+		"VK_LAYER_LUNARG_api_dump"
+	};
+	vk_create_info_.ppEnabledLayerNames = validationLayers;
+}
+
+
+void window::physical_devices()
+{
 	unsigned int n_devices = 0;
 	vkEnumeratePhysicalDevices(vk_instance_, &n_devices, nullptr);
 
@@ -75,74 +157,97 @@ window::window()
 		throw std::runtime_error("We could not find a GPU compatible with Vulkan.");
 	}
 
-	// --------------------------------------
 
-	// ......................................
+}
 
-	// Here we set the validation layers
+void window::logical_devices()
+{
+	indices = findQueueFamilies(vk_physical_device_);
 
-	uint32_t layerCount = 0;
-	vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
-	std::vector<VkLayerProperties> availableLayers(layerCount);
-	vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
+	VkDeviceQueueCreateInfo queueCreateInfo{};
+	queueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+	queueCreateInfo.queueFamilyIndex = indices.graphicsFamily.value();
+	queueCreateInfo.queueCount = 1;
 
-	vk_create_info_.enabledLayerCount = 2;
-	const char* validationLayers[] = {
-		"VK_LAYER_KHRONOS_validation",
-		"VK_LAYER_LUNARG_api_dump"
-	};
-	vk_create_info_.ppEnabledLayerNames = validationLayers;
+	float queuePriority = 1.0f;
+	queueCreateInfo.pQueuePriorities = &queuePriority;
 
-	for (const char* layerName : validationLayers) {
-		bool layerFound = false;
-		for (const auto& layerProperties : availableLayers) {
-			if (strcmp(layerName, layerProperties.layerName) == 0) {
-				layerFound = true;
-				break;
-			}
-		}
+	VkPhysicalDeviceFeatures deviceFeatures{};
+	VkDeviceCreateInfo createInfo{};
+	createInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+	createInfo.pQueueCreateInfos = &queueCreateInfo;
+	createInfo.queueCreateInfoCount = 1;
+	createInfo.enabledExtensionCount = 1;
+	const char* deviceExtensions[] = { VK_KHR_SWAPCHAIN_EXTENSION_NAME };
+	createInfo.ppEnabledExtensionNames = deviceExtensions;
 
-		if (!layerFound) {
-			throw std::runtime_error("Capa de validación solicitada no disponible: " + std::string(layerName));
+	createInfo.pEnabledFeatures = &deviceFeatures;
+
+
+	if (vkCreateDevice(vk_physical_device_, &createInfo, nullptr, &vk_device_) != VK_SUCCESS) {
+		throw std::runtime_error("failed to create logical device!");
+	}
+
+	if (glfwCreateWindowSurface(vk_instance_, window_, nullptr, &surface_) != VK_SUCCESS) {
+		throw std::runtime_error("Error while creating surface screen");
+	}
+
+	VkBool32 surfaceSupport = VK_FALSE;
+	vkGetPhysicalDeviceSurfaceSupportKHR(vk_physical_device_, indices.graphicsFamily.value(), surface_, &surfaceSupport);
+
+	if (surfaceSupport != VK_TRUE) {
+		throw std::runtime_error("El dispositivo físico no es compatible con la superficie de ventana.");
+	}
+}
+
+void window::create_presentation_queue_and_swapchain()
+{
+	vkGetPhysicalDeviceQueueFamilyProperties(vk_physical_device_, &indices.queueFamilyCount, nullptr);
+	
+
+	VkBool32 surfaceSupport = VK_FALSE;
+	for (uint32_t i = 0; i < indices.queueFamilyCount; ++i) {
+		if (vkGetPhysicalDeviceSurfaceSupportKHR(vk_physical_device_, i, surface_, &surfaceSupport) == VK_SUCCESS && surfaceSupport) {
+			vkGetDeviceQueue(vk_device_, i, 0, &graphicsQueue);
+			break;
 		}
 	}
 
+	uint32_t formatCount;
+	vkGetPhysicalDeviceSurfaceFormatsKHR(vk_physical_device_, surface_, &formatCount, nullptr);
+	
+	if (formatCount == 0) {
+		throw std::runtime_error("We could not find any surface formats compatible");
+	}
 
-}
+	std::vector<VkSurfaceFormatKHR> surfaceFormats(formatCount);
+	vkGetPhysicalDeviceSurfaceFormatsKHR(vk_physical_device_, surface_, &formatCount, surfaceFormats.data());
+	
+	selectedFormat = surfaceFormats[0];
 
-window::~window()
-{
-	vkDestroyInstance(vk_instance_, nullptr);
+	for (const VkSurfaceFormatKHR& availableFormat : surfaceFormats) {
+		if (availableFormat.format == VK_FORMAT_B8G8R8A8_UNORM &&
+			availableFormat.colorSpace == VK_COLOR_SPACE_SRGB_NONLINEAR_KHR) {
+			selectedFormat = availableFormat;
+			break;
+		}
+	}
 
-	glfwDestroyWindow(window_);
+	/*if (selectedFormat.format == VK_FORMAT_UNDEFINED) {
+		selectedFormat = surfaceFormats[0];
+	}*/
 
-	glfwTerminate();
-}
 
-
-void window::end()
-{
-	glfwDestroyWindow(window_);
-	glfwTerminate();
-}
-
-void window::clean()
-{
-	VkClearAttachment clearAttachment = {};
-	clearAttachment.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
-	clearAttachment.clearValue = clearColor;
-
-	VkClearRect clearRect = {};
-	clearRect.layerCount = 1;
-	clearRect.rect.extent.width = width_;
-	clearRect.rect.extent.height = height_;
-
-	//vkCmdClearAttachments()
 }
 
 GLFWwindow* window::get_window()
 {
 	return window_;
+}
+
+VkSurfaceKHR window::get_vulkan_surface()
+{
+	return surface_;
 }
 
 void window::set_clear_color(float r, float g, float b, float a)
